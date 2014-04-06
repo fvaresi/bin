0dffb7be23b806cb75094c3eaa01392f0dae4d3b
Fernando Varesi <fvaresi@gmail.com>
2014-04-06 12:01:44 -0300

    Adding openarena and passmenu

--- recordmyshow.sh
+++ recordmyshow.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#URL="http://usa15.ciudaddigital.com.uy:8000/carve?type=flash&type=mp3&showdigits=true&image=rar.gif&autostart=true"
 URL="http://usa15.ciudaddigital.com.uy/medios/CristalAM/audio/directo.pls"
 FILENAME="/media/Datos/audicion/$(date +"%Y%m%d_%H%M").mp3"
 LOG="$HOME/recordmyshow.log"
@@ -9,7 +8,6 @@ transmission-remote -tall --stop
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Started recording $URL" >>$LOG
 cvlc --quiet --run-time=7200 $URL --sout "#standard{access=file,mux=raw,dst=$FILENAME}" vlc://quit
-#cvlc --quiet --run-time=7200 $URL --sout "#standard{access=file,mux=asf,dst=$FILENAME}" vlc://quit
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Stopped recording" >>$LOG
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Starting all torrents" >>$LOG
